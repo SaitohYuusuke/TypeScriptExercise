8060531d1ef4fc9cbf2577425a8d4c2c993ec3bd
yuusuke saitoh <yuusuke.saitoh@jp.ricoh.com>
2018-01-17 14:51:19 +0900

    p126途中

--- text117_page/app.ts
+++ text117_page/app.ts
@@ -1,4 +1,16 @@
+QUnit.module("Test Uite1");
+QUnit.test("Test A",(assert)=>{
+    assert.ok(true, "This shouldn't fail")
+});
+
+QUnit.test("Test B", (assert) => {
+    assert.ok(1 ===1, "This shouldn't fail");
+    assert.ok(false,"This should fail");
+});
+
+/*
 let m = document.getElementById("message");
 if(m != null){
     m.innerHTML = "Hello!";
 }
+*/
